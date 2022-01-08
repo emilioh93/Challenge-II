f948a58a314e2665e09e6677ed22b250401dd9c7
Emilio Hurtado <emiliohurtado1@gmail.com>
2022-01-07 23:39:03 -0300

    Se agregó lógica para guardar y cargar datos de LS

--- js/principal.js
+++ js/principal.js
@@ -5,26 +5,47 @@ function Image(url, likes, date) {
 }
 
 const images = [];
+const formAddImage = document.getElementById("formAddImage");
 
 const getRandomLikes = (min, max) => {
   return Math.random() * (max - min) + min;
 };
 
+const updateImages = () => {
+  const localStorageImages = JSON.parse(localStorage.getItem("images"));
+  localStorageImages?.forEach((item, index) => {
+    document.getElementById("addedStoriesContainer").innerHTML += `
+                    <button class="story">
+                      <div class="profile">
+                        <img src=${item.url} alt="Historia ${index}" />
+                      </div>
+                    </button>
+                  `;
+  });
+};
+
 const addImage = (image) => {
   images.push(image);
   localStorage.setItem("images", JSON.stringify(images));
+  document.getElementById("addedStoriesContainer").innerHTML += `
+      <button class="story">
+        <div class="profile">
+          <img src=${image.url} alt="Historia de Social Rolling" />
+        </div>
+      </button>
+    `;
 };
 
 // Publish Button
-document
-  .getElementById("formAddImage")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    const url = document.getElementById("inputURLImage").value;
-    const likes = getRandomLikes(0, 20);
-    const currentDate = new Date();
+formAddImage.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const url = document.getElementById("inputURLImage").value;
+  const likes = getRandomLikes(0, 20);
+  const currentDate = new Date();
 
-    const newImg = new Image(url, parseInt(likes), currentDate);
-    addImage(newImg);
-    document.getElementById("formAddImage").reset();
-  });
+  const newImg = new Image(url, parseInt(likes), currentDate);
+  addImage(newImg);
+  formAddImage.reset();
+});
+
+updateImages();
