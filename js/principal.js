31d9f78826a080d24c348200e0b83e937173ec40
Emilio Hurtado <emiliohurtado1@gmail.com>
2022-01-08 17:52:16 -0300

    Se agregó funcionalidad para abrir modal de Story

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,5 @@
-function Image(url, likes, date) {
+function Image(id, url, likes, date) {
+  this.id = id;
   this.url = url;
   this.likes = likes;
   this.date = date;
@@ -15,7 +16,7 @@ const updateImages = () => {
   const localStorageImages = JSON.parse(localStorage.getItem("images"));
   localStorageImages?.forEach((item, index) => {
     document.getElementById("addedStoriesContainer").innerHTML += `
-                    <button class="story">
+                    <button class="story" data-bs-toggle="modal" data-bs-target="#storyModal${item.id}">
                       <div class="profile">
                         <img src=${item.url} alt="Historia ${index}" />
                       </div>
@@ -24,7 +25,7 @@ const updateImages = () => {
     document.getElementById("postsContainer").innerHTML += `
                   <div class="col-md-4 col-sm-12 postAdded">
                           <div class="card w-100">
-                            <img src=${item.url} class="card-img-top" alt="Post ${index}" />
+                            <img src=${item.url} class="card-img-top" alt="Posteo ${index}" />
                             <div class="card-body">
                               <div class="d-flex justify-content-between">
                                 <span>${item.date}</span>
@@ -34,13 +35,31 @@ const updateImages = () => {
                           </div>
                       </div>
                   `;
+    document.getElementById("storiesModalsContainer").innerHTML += `
+      <article class="modal fade" id="storyModal${item.id}" tabindex="-1" aria-labelledby="storyModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen-sm-down">
+          <div class="modal-content">
+            <div class="modal-body text-center">
+              <img src=${item.url} alt="Historia ${index}" />
+            </div>
+            <div class="modal-footer d-flex justify-content-between">
+              <div class="h3 text-warning"><i class="fas fa-minus-circle"></i></div>
+              <div class="d-flex flex-column">
+                <span class="h3"><i class="far fa-heart text-danger"></i> ${item.likes}</span>
+                <span class="text-secondary">${item.date}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    `;
   });
 };
 
 const addImage = (image) => {
   images.push(image);
   document.getElementById("addedStoriesContainer").innerHTML += `
-    <button class="story">
+    <button class="story" data-bs-toggle="modal" data-bs-target="#storyModal${image.id}">
       <div class="profile">
         <img src=${image.url} alt="Historia de Social Rolling" />
       </div>
@@ -59,6 +78,24 @@ const addImage = (image) => {
       </div>
     </div>
   `;
+  document.getElementById("storiesModalsContainer").innerHTML += `
+      <article class="modal fade" id="storyModal${image.id}" tabindex="-1" aria-labelledby="storyModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen-sm-down">
+          <div class="modal-content">
+            <div class="modal-body text-center">
+              <img src=${image.url} alt="Historia de Rolling Social" />
+            </div>
+            <div class="modal-footer d-flex justify-content-between">
+              <div class="h3 text-warning"><i class="fas fa-minus-circle"></i></div>
+              <div class="d-flex flex-column">
+                <span class="h3"><i class="far fa-heart text-danger"></i> ${image.likes}</span>
+                <span class="text-secondary">${image.date}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    `;
   addToLocalStorage();
 };
 
@@ -68,8 +105,9 @@ formAddImage.addEventListener("submit", function (e) {
   const url = document.getElementById("inputURLImage").value;
   const likes = getRandomLikes(0, 20);
   const currentDate = moment().format("l");
+  const id = Date.now();
 
-  const newImg = new Image(url, parseInt(likes), currentDate);
+  const newImg = new Image(id, url, parseInt(likes), currentDate);
   addImage(newImg);
   formAddImage.reset();
 });
