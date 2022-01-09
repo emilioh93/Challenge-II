8257cf29ab88a54004c0ac54eeabceaafec68af9
Emilio Hurtado <emiliohurtado1@gmail.com>
2022-01-08 23:19:43 -0300

    Se mejoró style de botón delete

--- js/builders.js
+++ js/builders.js
@@ -1,5 +1,5 @@
 const buildStory = (item) => {
-  document.getElementById("addedStoriesContainer").innerHTML += `
+    document.getElementById("addedStoriesContainer").innerHTML += `
       <button class="story" data-bs-toggle="modal" data-bs-target="#storyModal${item.id}">
         <div class="profile">
           <img src=${item.url} alt="Historia Social Rolling" />
@@ -9,10 +9,10 @@ const buildStory = (item) => {
 };
 
 const buildStoryModal = (item) => {
-  const dataStorage = JSON.parse(localStorage.getItem("images"));
-  const index = dataStorage?.findIndex((element) => element.id === item.id);
+    const dataStorage = JSON.parse(localStorage.getItem("images"));
+    const index = dataStorage.findIndex((element) => element.id === item.id);
 
-  document.getElementById("storiesModalsContainer").innerHTML += `
+    document.getElementById("storiesModalsContainer").innerHTML += `
         <article class="modal fade" id="storyModal${item.id}" tabindex="-1" aria-labelledby="storyModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-fullscreen-sm-down">
             <div class="modal-content">
@@ -20,7 +20,11 @@ const buildStoryModal = (item) => {
                 <img class="w-100" src=${item.url} alt="Historia Social Rolling" />
               </div>
               <div class="modal-footer d-flex justify-content-between">
-                <div class="h3 text-warning"><i class="fas fa-minus-circle" onclick="deleteImage(${item.id})"></i></div>
+                <div class="h3">
+                    <button onclick="deleteImage(${item.id})" class="likesButton">
+                        <i class="fas fa-minus-circle text-warning" ></i>
+                    </button>
+                </div>
                 <div class="d-flex flex-column">
                   <span id="likes${item.id}" class="h3">
                     <button onclick="addLike(${index})" class="likesButton">
@@ -37,7 +41,7 @@ const buildStoryModal = (item) => {
 };
 
 const buildPost = (item) => {
-  document.getElementById("postsContainer").innerHTML += `
+    document.getElementById("postsContainer").innerHTML += `
     <div class="col-md-4 col-sm-12 postAdded">
       <div class="card w-100">
         <img src=${item.url} class="card-img-top" alt="Post de Social Rolling" />
--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,9 @@
 // Constructor de Imagen
 function Image(id, url, likes, date) {
-  this.id = id;
-  this.url = url;
-  this.likes = likes;
-  this.date = date;
+    this.id = id;
+    this.url = url;
+    this.likes = likes;
+    this.date = date;
 }
 
 let images = [];
@@ -12,63 +12,71 @@ const localStorageImages = JSON.parse(localStorage.getItem("images"));
 
 // Función para generar likes de manera aleatoria
 const getRandomLikes = (min, max) => {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 };
 
 // Actualizar Local Storage
 const updateImages = () => {
-  localStorageImages.forEach((item) => {
-    buildStory(item);
-    buildPost(item);
-    buildStoryModal(item);
-  });
+    localStorageImages.forEach((item) => {
+        buildStory(item);
+        buildPost(item);
+        buildStoryModal(item);
+    });
 };
 
 // Agregar imagen a Local Storage
 const addToLocalStorage = () => {
-  localStorage.setItem("images", JSON.stringify(images));
+    localStorage.setItem("images", JSON.stringify(images));
 };
 
 // Agregar nueva imagen
 const addImage = (image) => {
-  images.push(image);
-  addToLocalStorage();
-  location.reload();
+    images.push(image);
+    addToLocalStorage();
+    location.reload();
 };
 
 // Evento disparado al presionar botón Publicar
-formAddImage.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const url = document.getElementById("inputURLImage").value;
-  const likes = getRandomLikes(0, 20);
-  const currentDate = moment().format("l");
-  const id = Date.now();
+formAddImage.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const url = document.getElementById("inputURLImage").value;
+    const likes = getRandomLikes(0, 20);
+    const currentDate = moment().format("l");
+    const id = Date.now();
 
-  const newImg = new Image(id, url, parseInt(likes), currentDate);
-  addImage(newImg);
-  formAddImage.reset();
+    const newImg = new Image(id, url, parseInt(likes), currentDate);
+    addImage(newImg);
+    formAddImage.reset();
 });
 
 // Eliminar imagen del Local Storage
 const deleteImage = (id) => {
-  const newLocalStorage = localStorageImages.filter((e) => e.id !== id);
-  localStorage.setItem("images", JSON.stringify(newLocalStorage));
-  location.reload();
+    const newLocalStorage = localStorageImages.filter((e) => e.id !== id);
+    localStorage.setItem("images", JSON.stringify(newLocalStorage));
+    location.reload();
 };
 
 // Incrementar likes
 const addLike = (index) => {
-  const itemInStorage = JSON.parse(localStorage.getItem("images"))[index];
-  itemInStorage.likes = itemInStorage?.likes + 1;
-  // FIXME: el problema está en que el ls se setea por un sólo elemento que pasa a ser objeto
-  localStorage.setItem("images", JSON.stringify(newLocalStorage));
+    // Obtener objeto
+    const itemInStorage = JSON.parse(localStorage.getItem("images"))[index];
+    // Sumarle like
+    itemInStorage.likes = itemInStorage.likes + 1;
+    // Guardarlo en un nuevo objeto
+    const newImage = itemInStorage;
+    // Eliminar el objeto anterior del array
+    deleteImage(itemInStorage.id);
+    // Agregar nuevo objeto al array
+    images.push(newImage);
+    // FIXME: el problema está en que el ls se setea por un sólo elemento que pasa a ser objeto
+    localStorage.setItem("images", JSON.stringify(images));
 };
 
 // Renderizar Local Storage al cargar página
 window.onload = () => {
-  console.log("🚀 ~ localStorageImages", localStorageImages);
-  if (localStorageImages) {
-    images = localStorageImages;
-    updateImages();
-  }
+    console.log("🚀 ~ localStorageImages", localStorageImages);
+    if (localStorageImages) {
+        images = localStorageImages;
+        updateImages();
+    }
 };
